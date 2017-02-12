8f0e4f355bdfdcf14a27c40c24af333c37714e49
Joe Wheatley <joewheatley7@gmail.com>
2017-02-12 01:10:46 +0000

    part 5: services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Tour of heroes';
-  heroes = HEROES;
+  heroes : Hero[];
   selectedHero: Hero;
+  constructor(private heroService: HeroService){}
+  ngOnInit(): void{
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
